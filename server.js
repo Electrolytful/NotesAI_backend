47e71f0469d93e912dbe8cc89d203fdce35ec7b0
Electrolytful <erhanulusal@hotmail.com>
2023-04-17 15:20:15 +0100

    added user routes, implemented auth via session and cookies

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const usersRoute = require("./routes/usersRoutes.js");
 
+// global variables
+const oneDay = 1000 * 60 * 60 * 24;
 
 // defining the port
 const port = process.env.PORT || 4000;
@@ -14,12 +18,25 @@ const app = express();
 // applying middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: { maxAge: oneDay },
+  })
+);
 
 // running the server on the specified port
 app.listen(port, () => {
-    console.log(`NotesAI server running on port: ${port}`);
+  console.log(`NotesAI server running on port: ${port}`);
 });
 
 app.get("/", (req, res) => {
-    res.status(200).send("Welcome to the NotesAI api!");
+  res.status(200).send("Welcome to the NotesAI api!");
 });
+
+// users route
+app.use("/users", usersRoute);
